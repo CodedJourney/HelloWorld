0137b0144e68fa1a542c710ce65d0b4ac8308e30
Coded Journey <coded.journey@gmail.com>
2020-06-22 16:08:45 -0700

    Testing variables and printing them to console

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,15 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        // Testing integers and expression
+        // Testing variables and printing them to the console
+        int myFirstInteger = 4*10;
+        int mySecondInteger = 6;
+        int myThirdInteger = myFirstInteger * 2;
+        int myTotal = myFirstInteger + mySecondInteger + myThirdInteger;
+        int myLastOne = myTotal - 1000;
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
     }
 }
